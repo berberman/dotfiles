fef6144ef3a78e4613749ab9c7e039b192440e11
berberman <1793913507@qq.com>
2020-10-04 10:03:51 +0800

    Update

--- xmonad.hs
+++ xmonad.hs
@@ -1,53 +1,72 @@
-import XMonad
-import XMonad.Config.Kde
-import qualified XMonad.StackSet as W
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.SpawnOnce
 import Data.Function ((&))
 import qualified Data.Map as M
+import XMonad
+import XMonad.Config.Kde
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeInactive
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.SpawnOnce
 
+main =
+  xmonad . ewmh . docks $
+    kde4Config
+      { modMask = mod4Mask,
+        focusFollowsMouse = False,
+        manageHook = manageHook kde4Config <+> myManageHook <+> manageDocks,
+        layoutHook = myLayoutHook,
+        handleEventHook = fullscreenEventHook,
+        workspaces = myWorkspaces,
+        logHook = myLogHook,
+        startupHook = startupHook kde4Config >> spawn "picom --config ~/.xmonad/picom.conf &",
+        borderWidth = 0,
+        terminal = "alacritty"
+      }
+      `additionalKeys` myKeys
 
-main = xmonad . ewmh . docks $ kde4Config
-    { modMask = mod4Mask -- use the Windows button as mod
-    , focusFollowsMouse = False
-    , manageHook = manageHook kde4Config <+> myManageHook <+> manageDocks
-    , layoutHook = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $ avoidStruts $ layoutHook kde4Config
-    , handleEventHook = fullscreenEventHook
-    , workspaces = named ++ map show [ (1 & (length named +))  .. 9]
-    , logHook = myLogHook
-    , startupHook = startupHook kde4Config >> spawn "picom --config ~/.xmonad/picom.conf &"
-    , borderWidth = 0
-    , terminal = "alacritty"
-    } `additionalKeys` myKeys
-      where
-        named = ["Web", "IM", "Code", "Proc", "Music"]
+myWorkspaces = let n = ["Web", "IM", "Code", "Proc", "Music"] in n ++ map show [(1 & (length n +)) .. 8]
 
-myKeys = [ ((mod4Mask, xK_r), spawn "dmenu_run")
-         , ((mod4Mask .|. controlMask, xK_r), spawn "xmonad --recompile && xmonad --restart")
-         , ((mod4Mask .|. controlMask, xK_l), spawn "killall latte-dock && kstart5 latte-dock")
-         , ((mod4Mask .|. controlMask, xK_p), mySpawnOn "Web" "chromium")
-         , ((mod4Mask .|. controlMask, xK_t), mySpawnOn "IM" "telegram-desktop")
-         , ((mod4Mask .|. controlMask, xK_e), mySpawnOn "IM" "element-desktop")
-         ]
+spac = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True . avoidStruts
 
-myManageHook = composeAll . concat $
-    [ [ className   =? c --> doFloat           | c <- myFloats]
-    , [ className   =? c --> doShift "IM"      | c <- imApps] 
-    , [ className   =? c --> doShift "Web"     | c <- webApps]
-    , [ name        =? "weechat"      --> doShift "IM"]
-    , [ name        =? "KRDC"         --> doShift "Remote"]
-    , [ name        =? "Media viewer" --> doFloat]
+myLayoutHook = spac (ThreeCol 1 (3 / 100) (1 / 2) ||| ThreeColMid 1 (3 / 100) (1 / 2) ||| Mirror (Tall 1 (3 / 100) (1 / 2)) ||| Full)
+
+myKeys =
+  [ ((mod4Mask, xK_r), spawn "dmenu_run"),
+    ((mod4Mask .|. controlMask, xK_r), spawn "xmonad --recompile && xmonad --restart"),
+    ((mod4Mask .|. controlMask, xK_l), spawn "killall latte-dock && kstart5 latte-dock"),
+    ((mod4Mask .|. controlMask, xK_p), mySpawnOn "Web" chromium),
+    ((mod4Mask .|. controlMask, xK_t), mySpawnOn "IM" tg),
+    ((mod4Mask .|. controlMask, xK_e), mySpawnOn "IM" element)
+  ]
+
+myManageHook =
+  composeAll . concat $
+    [ [className =? c --> doFloat | c <- floatByClass],
+      [className =? c --> doShift "IM" | c <- imApps],
+      [className =? c --> doShift "Web" | c <- webApps],
+      [className =? c --> doIgnore | c <- ignoreByClass],
+      [name =? "weechat" --> doShift "IM"],
+      [name =? c --> doFloat | c <- floatByName]
     ]
   where
-      name = stringProperty "WM_NAME"
-      myFloats      = ["lattedock", "yakuake", "jetbrains-toolbox"]
-      webApps       = ["chromium"]
-      imApps        = ["telegram-desktop", "slack", "element"]
-   
+    name = stringProperty "WM_NAME"
+    floatByClass = ["lattedock", "yakuake", "jetbrains-toolbox"]
+    floatByName = ["Media viewer"]
+    ignoreByClass = ["plasmashell"]
+    webApps = [chromium]
+    imApps = [tg, slack, element]
+
+chromium = "chromium"
+
+tg = "telegram-desktop"
+
+element = "element-desktop"
+
+slack = "slack"
+
 myLogHook :: X ()
 myLogHook = fadeInactiveLogHook 1.0
 
