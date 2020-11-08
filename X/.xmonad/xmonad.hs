68698860df60c5ff2a51cb15cff41a3c3d1ceff4
Potato Hatsue <1793913507@qq.com>
2020-11-08 16:10:37 +0800

    Update picom

--- X/.xmonad/xmonad.hs
+++ X/.xmonad/xmonad.hs
@@ -21,7 +21,7 @@ main =
         handleEventHook = fullscreenEventHook,
         workspaces = myWorkspaces,
         logHook = myLogHook,
-        startupHook = startupHook kde4Config >> spawn "picom &",
+        startupHook = startupHook kde4Config >> spawn "picom --experimental-backends &",
         borderWidth = 0,
         terminal = "alacritty"
       }
@@ -35,7 +35,8 @@ myLayoutHook = spac (ThreeCol 1 (3 / 100) (1 / 2) ||| ThreeColMid 1 (3 / 100) (1
 
 myKeys =
   [ ((mod4Mask, xK_r), spawn "rofi -combi-modi drun -theme Adapta-Nokto -show combi -show-icons"),
-    ((mod4Mask .|. controlMask, xK_r), spawn "xmonad --recompile && xmonad --restart"),
+    ((mod4Mask .|. controlMask, xK_r), spawn "xmonad --recompile && xmonad --restart" 
+        >> spawn "killall picom && picom --experimental-backends &"),
     ((mod4Mask .|. controlMask, xK_l), spawn "killall latte-dock && kstart5 latte-dock"),
     ((mod4Mask .|. controlMask, xK_p), mySpawnOn "Web" chromium),
     ((mod4Mask .|. controlMask, xK_t), mySpawnOn "IM" tg),
